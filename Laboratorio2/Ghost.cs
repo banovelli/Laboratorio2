41f4a4fcbe7816359d535aaa65996e8ab15fc543
banovelli <bnovelli@gmail.com>
2016-07-08 11:30:41 -0300

    add class ghost

---
+++ Laboratorio2/Ghost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    public class Ghost : Enemy
+    {
+        public Ghost(Game game, Point location, Rectangle bounderies)
+            : base(game, location, bounderies, 8)
+        { }
+
+        public override void Move(Random random)
+        {
+            //se esta vivo
+
+            //1 em 3 de chance de mover ao jogador ou 2 em 3 q ele fique parado
+
+            //apos mover , se proximo ataca com ate 3 pontos de dano
+        }
+    }
+}
