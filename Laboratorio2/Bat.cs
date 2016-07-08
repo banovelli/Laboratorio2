74b2e20791f83bf197b758c5ad60ca1c5aa59f37
banovelli <bnovelli@gmail.com>
2016-07-08 11:28:11 -0300

    add class bat

---
+++ Laboratorio2/Bat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    public class Bat : Enemy
+    {
+        public Bat(Game game, Point location, Rectangle bounderies)
+            : base (game, location, bounderies, 6)
+        {}
+
+        public override void Move(Random random)
+        {
+         //se esta vivo
+   
+            //50% de chance de mover ao jogador ou dir aleatoria
+
+            //apos mover , se proximo ataca com ate 2 pontos de dano
+        }
+    }
+}
