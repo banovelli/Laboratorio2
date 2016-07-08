78b43104d29f1971ad00138c25fbfcc037867f37
banovelli <bnovelli@gmail.com>
2016-07-08 11:23:49 -0300

    add class enemy

---
+++ Laboratorio2/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    public class Enemy: Mover
+    {
+        private const int NearPlayerDistance = 25;
+        private int hitPoints;
+        public int HitPoints { get { return hitPoitns; } }
+        public bool Dead
+        {
+            get
+            {
+                if (hitPoints <= 0) return true;
+                else return false;
+            }
+        }
+
+        public Enemy(Game game, Point location, Rectangle bounderies, int hitPoints)
+            : base(game, location)
+        {
+            this.hitPoints = hitPoints;
+        }
+
+        public abstract void Move(Random random);
+        public void Hit(int maxDamage, Random random)
+        {
+            hitPoints -= random.Next(1, maxDamage);
+        }
+
+        protected bool NearPlayer()
+        {
+            return (Nearby(game.PlayerLocation, NearPlayerDistance));
+        }
+
+        protected Direction FindPlayerDirection(Point playerLocation)
+        {
+            Direction directionToMove;
+            if (playerLocation.X > location.X + 10)
+                directionToMove = Direction.Right;
+            else if (playerLocation.X < location.X - 10)
+                directionToMove = Direction.Left;
+            else if (playerLocation.Y < location.Y - 10)
+                directionToMove = Direction.Up;
+            else
+                directionToMove = Direction.Down;
+
+            return directionToMove;
+        }
+
+
+    }
+}
